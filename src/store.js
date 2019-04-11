cb2008c93e07c5413717635993d7329fe8e7ebbc
Luwei <lw1023@msn.com>
2019-04-11 15:02:20 +0800

    添加axios

--- src/store.js
+++ src/store.js
@@ -4,13 +4,26 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state: {
-
+    count: 0,
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    }
   },
   mutations: {
-
+    increment(state) {
+      state.count++
+    }
   },
-  actions: {
-
-  }
+  actions: {}
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <span>{{ countAlias }}</span>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      localCount: 100,
+    }
+  },
+  // computed: {
+  //   count () {
+  //     return this.$store.state.count
+  //   }
+  // },
+  computed: mapState({
+    // 箭头函数可使代码更简练
+    count: state => state.count,
+
+    // 传字符串参数 'count' 等同于 `state => state.count`
+    countAlias: 'count',
+
+    // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+    countPlusLocalState (state) {
+      return state.count + this.localCount
+    }
+  }),
+  beforeRouteEnter (to, from, next) {
+    // 在渲染该组件的对应路由被 confirm 前调用
+    // 不！能！获取组件实例 `this`
+    // 因为当守卫执行前，组件实例还没被创建
+    next(vm => {
+      // 通过 `vm` 访问组件实例
+      vm.$store.commit('increment')
+    })
+  },
+  // beforeRouteUpdate (to, from, next) {
+  //   // 在当前路由改变，但是该组件被复用时调用
+  //   // 举例来说，对于一个带有动态参数的路径 /foo/:id，在 /foo/1 和 /foo/2 之间跳转的时候，
+  //   // 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个钩子就会在这个情况下被调用。
+  //   // 可以访问组件实例 `this`
+  // },
+  // beforeRouteLeave (to, from, next) {
+  //   // 导航离开该组件的对应路由时调用
+  //   // 可以访问组件实例 `this`
+  // }
+}
+</script>
+
